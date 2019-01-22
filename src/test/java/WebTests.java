32aa3342073c01f346f53558934cb0681356a27c
palotas <michael.palotas@element34.com>
2019-01-22 16:23:08 +0100

    refactored to new class and using testng

--- src/test/java/WebTests.java
+++ src/test/java/WebTests.java
@@ -1,11 +1,13 @@
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -13,15 +15,22 @@ import java.util.concurrent.TimeUnit;
 
 public class WebTests {
 
-    @Test
-    public void webtest() throws MalformedURLException {
+    public RemoteWebDriver driver = null;
+    public WebDriverWait wait = null;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName("chrome");
+        capabilities.setCapability("e34:token", "30e75a42-817d-4c"); //mpalotas
+        capabilities.setCapability("e34:video", true);
+        driver = new RemoteWebDriver(new URL("http://vm-106.element34.net/wd/hub"), capabilities);
+        wait = new WebDriverWait(driver, 5000);
 
+    }
 
-        RemoteWebDriver driver = new RemoteWebDriver(new URL("https://<URL-OF-SELENIUMBOX>"), capabilities);
-        WebDriverWait wait = new WebDriverWait(driver, 5000);
-
+    @Test
+    public void webtest() throws MalformedURLException {
         driver.get("https://www.bmw.de/de/index.html");
         driver.findElement(By.linkText("Alle BMW Modelle")).click();
         wait.until((ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#ds2-model-page > div.row > div > h1"))));
---
+++ src/test/java/WinAppDriverTest.java
@@ -0,0 +1,115 @@
+import io.appium.java_client.windows.WindowsDriver;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class WinAppDriverTest {
+
+    private static WindowsDriver calculatorSession = null;
+    private static WebElement calculatorResult = null;
+
+    @BeforeClass
+    public static void setup() throws MalformedURLException {
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+            calculatorSession = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
+            calculatorSession.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+            calculatorResult = calculatorSession.findElementByAccessibilityId("CalculatorResults");
+            Assert.assertNotNull(calculatorResult);
+    }
+
+
+    @BeforeMethod
+    public void clear()
+    {
+        calculatorSession.findElementByName("Clear").click();
+        Assert.assertEquals("0", getCalculatorResultText());
+    }
+
+
+    @Test
+    public void Addition()
+    {
+        calculatorSession.findElementByName("One").click();
+        calculatorSession.findElementByName("Plus").click();
+        calculatorSession.findElementByName("Seven").click();
+        calculatorSession.findElementByName("Equals").click();
+        Assert.assertEquals("8", getCalculatorResultText());
+    }
+
+
+    @Test
+    public void Combination()
+    {
+        calculatorSession.findElementByName("Seven").click();
+        calculatorSession.findElementByName("Multiply by").click();
+        calculatorSession.findElementByName("Nine").click();
+        calculatorSession.findElementByName("Plus").click();
+        calculatorSession.findElementByName("One").click();
+        calculatorSession.findElementByName("Equals").click();
+        calculatorSession.findElementByName("Divide by").click();
+        calculatorSession.findElementByName("Eight").click();
+        calculatorSession.findElementByName("Equals").click();
+        org.junit.Assert.assertEquals("8", getCalculatorResultText());
+    }
+
+    @Test
+    public void Division()
+    {
+        calculatorSession.findElementByName("Eight").click();
+        calculatorSession.findElementByName("Eight").click();
+        calculatorSession.findElementByName("Divide by").click();
+        calculatorSession.findElementByName("One").click();
+        calculatorSession.findElementByName("One").click();
+        calculatorSession.findElementByName("Equals").click();
+        org.junit.Assert.assertEquals("8", getCalculatorResultText());
+    }
+
+    @Test
+    public void Multiplication()
+    {
+        calculatorSession.findElementByName("Nine").click();
+        calculatorSession.findElementByName("Multiply by").click();
+        calculatorSession.findElementByName("Nine").click();
+        calculatorSession.findElementByName("Equals").click();
+        org.junit.Assert.assertEquals("81", getCalculatorResultText());
+    }
+
+    @Test
+    public void Subtraction()
+    {
+        calculatorSession.findElementByName("Nine").click();
+        calculatorSession.findElementByName("Minus").click();
+        calculatorSession.findElementByName("One").click();
+        calculatorSession.findElementByName("Equals").click();
+        org.junit.Assert.assertEquals("8", getCalculatorResultText());
+    }
+
+
+    protected String getCalculatorResultText()
+    {
+        // trim extra text and whitespace off of the display value
+        return calculatorResult.getText().replace("Display is", "").trim();
+    }
+
+    @AfterTest
+    public static void TearDown()
+    {
+        calculatorResult = null;
+        if (calculatorSession != null) {
+            calculatorSession.quit();
+        }
+        calculatorSession = null;
+    }
+
+
+
+}
